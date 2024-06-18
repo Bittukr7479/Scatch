b268cccd167988aa01935724d490ab342b323ca3
Bittu kumar <kumarbittuu26@gmail.com>
2024-06-19 00:24:08 +0530

    "Production setup"

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,12 +1,14 @@
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
+const dbgr = require("debug")("development:mongoose");
+const config = require("config");
 
 mongoose
-.connect("mongodb://127.0.0.1:27017/scatch")
-.then(function () {
-    console.log("connected");
-})
-.catch(function (err) {
-    console.log(err);
-})
+  .connect(`${config.get("MONGODB_URI")}/scatch`)
+  .then(function () {
+    dbgr("connected");
+  })
+  .catch(function (err) {
+    dbgr(err);
+  });
 
 module.exports = mongoose.connection;
